8ef8eb4fb3afd8f9d15a7c4a7397bcce4f42656a
Ming Lei <tom.leiming@gmail.com>
2022-07-02 09:53:00 +0000

    ublksrv: move public data structure into ublksrv.h
    ublksrv_tgt.h will hold target related data structure
    and function declaim, which can be thought as private
    from libublksrv viewpoint.
    
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

--- ublksrv.h
+++ ublksrv.h
@@ -43,7 +43,7 @@ extern "C" {
 #endif
 
 #include "ublk_cmd.h"
-#include "ublksrv_tgt.h"
+
 
 #define MAX_NR_UBLK_DEVS	128
 
@@ -146,6 +146,47 @@ struct ublksrv_queue {
 	struct ublksrv_dev *dev;
 
 	struct ublk_io ios[0];
+};
+
+#define  UBLKSRV_TGT_MAX_FDS	8
+enum {
+	/* evaluate communication cost, ublksrv_null vs /dev/nullb0 */
+	UBLKSRV_TGT_TYPE_NULL,
+
+	/* ublksrv_loop vs. /dev/loop */
+	UBLKSRV_TGT_TYPE_LOOP,
+
+	UBLKSRV_TGT_TYPE_MAX = 256,
+};
+
+struct ublksrv_tgt_type;
+
+struct ublksrv_tgt_info {
+	unsigned long long dev_size;
+	unsigned int tgt_ring_depth;	/* at most in-flight ios */
+	unsigned int nr_fds;
+	int fds[UBLKSRV_TGT_MAX_FDS];
+	void *tgt_data;
+	const struct ublksrv_tgt_type *ops;
+};
+
+struct ublksrv_tgt_type {
+	int  type;
+	const char *name;
+	int (*init_tgt)(struct ublksrv_tgt_info *, int type, int argc,
+			char *argv[]);
+	/*
+	 * c++20 coroutine is stackless, and can't be nested, so any
+	 * functions called from ->handle_io_async can't be defined as
+	 * coroutine, and please keep it in mind.
+	 */
+	int (*handle_io_async)(struct ublksrv_queue *, int tag);
+
+	void (*tgt_io_done)(struct ublksrv_queue *, struct io_uring_cqe *);
+
+	void (*usage_for_add)(void);
+
+	void (*deinit_tgt)(struct ublksrv_tgt_info *, struct ublksrv_dev *);
 };
 
 struct ublksrv_dev {
@@ -282,6 +323,12 @@ extern struct ublksrv_queue *ublksrv_queue_init(struct ublksrv_dev *dev,
 extern void ublksrv_queue_deinit(struct ublksrv_queue *q);
 
 extern int ublksrv_process_io(struct ublksrv_queue *q, unsigned *submitted);
+
+extern int ublksrv_register_tgt_type(struct ublksrv_tgt_type *type);
+extern void ublksrv_unregister_tgt_type(struct ublksrv_tgt_type *type);
+extern void ublksrv_for_each_tgt_type(void (*handle_tgt_type)(unsigned idx,
+			const struct ublksrv_tgt_type *type, void *data),
+		void *data);
 
 #ifdef __cplusplus
 }
