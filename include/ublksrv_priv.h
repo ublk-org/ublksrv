ublk-org/ublksrv

commit 3cdfe83133f23a4f657c211c570da53b2a5d3ca1

ublksrv: add 'dev_blocks' parameter to ublksrv_ctrl_start_dev
Signed-off-by: Ming Lei <tom.leiming@gmail.com>

ublksrv_tgt.c
@@ -250,6 +250,27 @@ static int ublksrv_start_daemon(struct ublksrv_ctrl_dev *ctrl_dev)
 	return daemon_pid;
 }
 
+static unsigned long long get_dev_blocks(struct ublksrv_ctrl_dev *ctrl_dev)
+{
+	unsigned long long dev_blocks;
+
+	if (ctrl_dev->dev_info.flags[0] & (1 << UBLK_F_HAS_IO_DAEMON)) {
+		char *addr;
+		int fd = ublksrv_open_shm(ctrl_dev, &addr);
+
+		if (fd > 0) {
+			const struct ublksrv_ctrl_dev_info *info =
+				(struct ublksrv_ctrl_dev_info *)addr;
+			dev_blocks = info->dev_blocks;
+			ublksrv_close_shm(ctrl_dev, fd, addr);
+		}
+	} else {
+		dev_blocks = ctrl_dev->dev_info.dev_blocks;
+	}
+
+	return dev_blocks;
+}
+
 static int cmd_dev_add(int argc, char *argv[])
 {
 	static const struct option longopts[] = {
@@ -317,7 +338,7 @@ static int cmd_dev_add(int argc, char *argv[])
 	if (ret <= 0)
 		goto fail_del_dev;
 
-	ret = ublksrv_ctrl_start_dev(dev, ret);
+	ret = ublksrv_ctrl_start_dev(dev, ret, get_dev_blocks(dev));
 	if (ret < 0) {
 		fprintf(stderr, "start dev failed %d, ret %d\n", data.dev_id,
 				ret);

ublksrv_cmd.c
@@ -157,7 +157,7 @@ int ublksrv_ctrl_get_affinity(struct ublksrv_ctrl_dev *ctrl_dev)
 	return 0;
 }
 
-static int ublksrv_open_shm(struct ublksrv_ctrl_dev *ctrl_dev, char
+int ublksrv_open_shm(struct ublksrv_ctrl_dev *ctrl_dev, char
 		**shm_addr)
 {
 	struct ublksrv_ctrl_dev_info *info = &ctrl_dev->dev_info;
@@ -178,7 +178,7 @@ static int ublksrv_open_shm(struct ublksrv_ctrl_dev *ctrl_dev, char
 	return fd;
 }
 
-static void ublksrv_close_shm(struct ublksrv_ctrl_dev *ctrl_dev, int fd,
+void ublksrv_close_shm(struct ublksrv_ctrl_dev *ctrl_dev, int fd,
 		char *shm_addr)
 {
 	munmap(shm_addr, UBLKSRV_SHM_SIZE);
@@ -201,7 +201,8 @@ static void ublksrv_close_shm(struct ublksrv_ctrl_dev *ctrl_dev, int fd,
  * /dev/ublk-control with device id, which will cause ublk driver to
  * expose /dev/ublkbN
  */
-int ublksrv_ctrl_start_dev(struct ublksrv_ctrl_dev *ctrl_dev, int daemon_pid)
+int ublksrv_ctrl_start_dev(struct ublksrv_ctrl_dev *ctrl_dev,
+		int daemon_pid, unsigned long long dev_blocks)
 {
 	struct ublksrv_ctrl_cmd_data data = {
 		.cmd_op	= UBLK_CMD_START_DEV,
@@ -210,20 +211,7 @@ int ublksrv_ctrl_start_dev(struct ublksrv_ctrl_dev *ctrl_dev, int daemon_pid)
 	int ret;
 
 	ctrl_dev->dev_info.ublksrv_pid = data.data[0] = daemon_pid;
-	if (ctrl_dev->dev_info.flags[0] & (1 << UBLK_F_HAS_IO_DAEMON)) {
-		char *addr;
-		int fd = ublksrv_open_shm(ctrl_dev, &addr);
-
-		if (fd > 0) {
-			struct ublksrv_ctrl_dev_info *info =
-				(struct ublksrv_ctrl_dev_info *)addr;
-			ctrl_dev->dev_info.dev_blocks = data.data[1] =
-				info->dev_blocks;
-			ublksrv_close_shm(ctrl_dev, fd, addr);
-		}
-	} else {
-		data.data[1] = ctrl_dev->dev_info.dev_blocks;
-	}
+	ctrl_dev->dev_info.dev_blocks = data.data[1] = dev_blocks;
 
 	ret = __ublksrv_ctrl_cmd(ctrl_dev, &data);
 

ublksrv_priv.h
@@ -8,6 +8,11 @@
 extern "C" {
 #endif
 
+extern int ublksrv_open_shm(struct ublksrv_ctrl_dev *ctrl_dev, char
+		**shm_addr);
+extern void ublksrv_close_shm(struct ublksrv_ctrl_dev *ctrl_dev, int fd,
+		char *shm_addr);
+
 static inline void ublksrv_mark_io_done(struct ublk_io *io, int res)
 {
 	/*

ublksrv.h
@@ -237,7 +237,8 @@ extern int ublksrv_ctrl_del_dev(struct ublksrv_ctrl_dev *dev);
 extern int ublksrv_ctrl_get_info(struct ublksrv_ctrl_dev *dev);
 extern int ublksrv_ctrl_stop_dev(struct ublksrv_ctrl_dev *dev);
 extern void ublksrv_ctrl_dump(struct ublksrv_ctrl_dev *dev);
-extern int ublksrv_ctrl_start_dev(struct ublksrv_ctrl_dev *ctrl_dev, int daemon_pid);
+extern int ublksrv_ctrl_start_dev(struct ublksrv_ctrl_dev *ctrl_dev,
+		int daemon_pid, unsigned long long dev_blocks);
 
 extern struct ublksrv_dev *ublksrv_dev_init(const struct ublksrv_ctrl_dev *
 		ctrl_dev);

commit ccb7a2e6dffe6dbf7decf3534a9a3fdd36bd9c15

ublksrv: fix disk size setup during starting device
ublksrv_open_shm() depends on valid info->ubdsrv_pid which is only
done inside ublksrv_ctrl_start_device(), so get_dev_blocks() has no
way to run ublksrv_open_shm() successfully.

Add 'daemon_pid' parameter to ublksrv_open_shm() for fixing the issue.

Meantime make sure that ublksrv_init_dev() is called before creating PID
file, so that get_dev_blocks() can observe disk size reliably.

Fixes: 3cdfe83 ("ublksrv: add 'dev_blocks' parameter to ublksrv_ctrl_start_dev")
Signed-off-by: Ming Lei <tom.leiming@gmail.com>

include/ublksrv_priv.h
@@ -9,7 +9,7 @@ extern "C" {
 #endif
 
 extern int ublksrv_open_shm(struct ublksrv_ctrl_dev *ctrl_dev, char
-		**shm_addr);
+		**shm_addr, int daemon_pid);
 extern void ublksrv_close_shm(struct ublksrv_ctrl_dev *ctrl_dev, int fd,
 		char *shm_addr);
 

lib/ublksrv_cmd.c
@@ -158,17 +158,23 @@ int ublksrv_ctrl_get_affinity(struct ublksrv_ctrl_dev *ctrl_dev)
 }
 
 int ublksrv_open_shm(struct ublksrv_ctrl_dev *ctrl_dev, char
-		**shm_addr)
+		**shm_addr, int daemon_pid)
 {
 	struct ublksrv_ctrl_dev_info *info = &ctrl_dev->dev_info;
 	int fd;
 	char buf[128];
 	char *addr;
 
-	if (info->ublksrv_pid <= 0)
+        if (!(info->flags[0] & (1 << UBLK_F_HAS_IO_DAEMON)))
 		return -EINVAL;
 
-	snprintf(buf, 128, "%s_%d", UBLKSRV_SHM_DIR, info->ublksrv_pid);
+	if (daemon_pid <= 0)
+		daemon_pid = info->ublksrv_pid;
+
+	if (daemon_pid <= 0)
+		return -EINVAL;
+
+	snprintf(buf, 128, "%s_%d", UBLKSRV_SHM_DIR, daemon_pid);
 	fd = shm_open(buf, O_RDONLY, 0);
 	if (fd <= 0)
 		return -EINVAL;
@@ -302,7 +308,7 @@ void ublksrv_ctrl_dump(struct ublksrv_ctrl_dev *dev)
 
         if (info->flags[0] & (1 << UBLK_F_HAS_IO_DAEMON)) {
 		char *addr;
-		int fd = ublksrv_open_shm(dev, &addr);
+		int fd = ublksrv_open_shm(dev, &addr, 0);
 
 		if (fd > 0) {
 			addr += sizeof(struct ublksrv_ctrl_dev_info);

ublksrv_tgt.c
@@ -131,20 +131,26 @@ static void ublksrv_io_handler(void *data)
 
 	syslog(LOG_INFO, "start ublksrv io daemon");
 
-	if (ublksrv_create_pid_file(dev_id))
-		return;
+	dev = ublksrv_dev_init(ctrl_dev);
+	if (!dev) {
+		syslog(LOG_ERR, "dev-%d start ubsrv failed", dev_id);
+		goto out;
+	}
+
+	/*
+	 * has to be called after ublksrv_dev_init returns, so that
+	 * the control task can observe disk size configured
+	 */
+	if (ublksrv_create_pid_file(dev_id)) {
+		syslog(LOG_ERR, "dev-%d create pid file failed", dev_id);
+		goto out_dev_deinit;
+	}
 
 	setup_pthread_sigmask();
 
 	info_array = (struct ublksrv_queue_info *)calloc(sizeof(
 				struct ublksrv_queue_info),
 			ctrl_dev->dev_info.nr_hw_queues);
-
-	dev = ublksrv_dev_init(ctrl_dev);
-	if (!dev) {
-		syslog(LOG_ERR, "start ubsrv failed");
-		goto out;
-	}
 
 	for (i = 0; i < ctrl_dev->dev_info.nr_hw_queues; i++) {
 		info_array[i].dev = dev;
@@ -156,13 +162,12 @@ static void ublksrv_io_handler(void *data)
 
 	/* wait until we are terminated */
 	ublksrv_drain_fetch_commands(dev, info_array);
-
-	ublksrv_dev_deinit(dev);
-
 	free(info_array);
-
- out:
 	ublksrv_remove_pid_file(dev_id);
+
+out_dev_deinit:
+	ublksrv_dev_deinit(dev);
+out:
 	syslog(LOG_INFO, "end ublksrv io daemon");
 	closelog();
 }
@@ -250,19 +255,23 @@ static int ublksrv_start_daemon(struct ublksrv_ctrl_dev *ctrl_dev)
 	return daemon_pid;
 }
 
-static unsigned long long get_dev_blocks(struct ublksrv_ctrl_dev *ctrl_dev)
+static unsigned long long get_dev_blocks(struct ublksrv_ctrl_dev *ctrl_dev,
+		int daemon_pid)
 {
-	unsigned long long dev_blocks;
+	unsigned long long dev_blocks = 0;
 
 	if (ctrl_dev->dev_info.flags[0] & (1 << UBLK_F_HAS_IO_DAEMON)) {
 		char *addr;
-		int fd = ublksrv_open_shm(ctrl_dev, &addr);
+		int fd = ublksrv_open_shm(ctrl_dev, &addr, daemon_pid);
 
 		if (fd > 0) {
 			const struct ublksrv_ctrl_dev_info *info =
 				(struct ublksrv_ctrl_dev_info *)addr;
 			dev_blocks = info->dev_blocks;
 			ublksrv_close_shm(ctrl_dev, fd, addr);
+		} else {
+			fprintf(stderr, "can't open shmem %d\n",
+					ctrl_dev->dev_info.dev_id);
 		}
 	} else {
 		dev_blocks = ctrl_dev->dev_info.dev_blocks;
@@ -338,7 +347,7 @@ static int cmd_dev_add(int argc, char *argv[])
 	if (ret <= 0)
 		goto fail_del_dev;
 
-	ret = ublksrv_ctrl_start_dev(dev, ret, get_dev_blocks(dev));
+	ret = ublksrv_ctrl_start_dev(dev, ret, get_dev_blocks(dev, ret));
 	if (ret < 0) {
 		fprintf(stderr, "start dev failed %d, ret %d\n", data.dev_id,
 				ret);