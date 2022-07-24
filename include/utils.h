c5d19c7712840ef1c8ede4e5821ef255a57de2a5
Ming Lei <tom.leiming@gmail.com>
2022-07-24 15:50:45 +0000

    ublksrv_tgt: move start_daemon into ublksrv_tgt.c
    So far only ublksrv_tgt.c uses start_daemon().
    
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

--- include/utils.h
+++ include/utils.h
@@ -5,16 +5,6 @@
 extern "C" {
 #endif
 
-/* Bit-mask values for 'flags' argument of create_daemon() */
-#define BD_NO_CHDIR           01    /* Don't chdir("/") */
-#define BD_NO_CLOSE_FILES     02    /* Don't close all open files */
-#define BD_NO_REOPEN_STD_FDS  04    /* Don't reopen stdin, stdout, and
-                                       stderr to /dev/null */
-#define BD_NO_UMASK0         010    /* Don't do a umask(0) */
-#define BD_MAX_CLOSE  8192          /* Maximum file descriptors to close if
-                                       sysconf(_SC_OPEN_MAX) is indeterminate */
-int start_daemon(int flags, void (*child_entry)(void *), void *data);
-
 /* create pid file */
 #define CPF_CLOEXEC 1
 int create_pid_file(const char *pidFile, int flags, int *pid_fd);
