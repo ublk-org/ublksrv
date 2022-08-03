f210c108506c1df711e6ff83577269b2ec93a906
Ming Lei <tom.leiming@gmail.com>
2022-08-03 10:44:03 -0400

    ublksrv_tgt.c: re-write start_daemon()
    So that start_deamon() becomes clean and simple.
    
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

--- ublksrv_tgt.c
+++ ublksrv_tgt.c
@@ -44,65 +44,30 @@ static char *pop_cmd(int *argc, char *argv[])
 	return cmd;
 }
 
-/* Bit-mask values for 'flags' argument of create_daemon() */
-#define BD_NO_CHDIR           01    /* Don't chdir("/") */
-#define BD_NO_CLOSE_FILES     02    /* Don't close all open files */
-#define BD_NO_REOPEN_STD_FDS  04    /* Don't reopen stdin, stdout, and
-                                       stderr to /dev/null */
-#define BD_NO_UMASK0         010    /* Don't do a umask(0) */
-#define BD_MAX_CLOSE  8192          /* Maximum file descriptors to close if
-                                       sysconf(_SC_OPEN_MAX) is indeterminate */
-int start_daemon(int flags, void (*child_entry)(void *), void *data)
+static int start_daemon(void (*child_fn)(void *), void *data)
 {
-    int maxfd, fd;
-#if 0
-    switch (fork()) {                   /* Become background process */
-    case -1: return -1;
-    case 0:  break;                     /* Child falls through... */
-    default: _exit(EXIT_SUCCESS);       /* while parent terminates */
-    }
-#endif
+	int fd;
 
-    if (setsid() == -1)                 /* Become leader of new session */
-        return -1;
+	if (setsid() == -1)
+		return -1;
 
-    switch (fork()) {                   /* Ensure we are not session leader */
-    case -1: return -1;
-    case 0:  break;
-    default: _exit(EXIT_SUCCESS);
-    }
+	switch (fork()) {
+	case -1: return -1;
+	case 0:  break;
+	default: _exit(EXIT_SUCCESS);
+	}
 
-    if (!(flags & BD_NO_UMASK0))
-        umask(0);                       /* Clear file mode creation mask */
+	close(STDIN_FILENO);
+	fd = open("/dev/null", O_RDWR);
+	if (fd != STDIN_FILENO)
+		return -1;
+	if (dup2(fd, STDOUT_FILENO) != STDOUT_FILENO)
+		return -1;
+	if (dup2(fd, STDERR_FILENO) != STDERR_FILENO)
+		return -1;
 
-    if (!(flags & BD_NO_CHDIR))
-        chdir("/");                     /* Change to root directory */
-
-    if (!(flags & BD_NO_CLOSE_FILES)) { /* Close all open files */
-        maxfd = sysconf(_SC_OPEN_MAX);
-        if (maxfd == -1)                /* Limit is indeterminate... */
-            maxfd = BD_MAX_CLOSE;       /* so take a guess */
-
-        for (fd = 0; fd < maxfd; fd++)
-            close(fd);
-    }
-
-    if (!(flags & BD_NO_REOPEN_STD_FDS)) {
-        close(STDIN_FILENO);            /* Reopen standard fd's to /dev/null */
-
-        fd = open("/dev/null", O_RDWR);
-
-        if (fd != STDIN_FILENO)         /* 'fd' should be 0 */
-            return -1;
-        if (dup2(STDIN_FILENO, STDOUT_FILENO) != STDOUT_FILENO)
-            return -1;
-        if (dup2(STDIN_FILENO, STDERR_FILENO) != STDERR_FILENO)
-            return -1;
-    }
-
-    child_entry(data);
-
-    return 0;
+	child_fn(data);
+	return 0;
 }
 
 char *__ublksrv_tgt_return_json_buf(struct ublksrv_dev *dev, int *size)
@@ -333,7 +298,7 @@ out:
 /* Not called from ublksrv daemon */
 static int ublksrv_start_io_daemon(const struct ublksrv_ctrl_dev *dev)
 {
-	start_daemon(0, ublksrv_io_handler, (void *)dev);
+	start_daemon(ublksrv_io_handler, (void *)dev);
 	return 0;
 }
 
