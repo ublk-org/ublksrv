84c78d15555a549691e920aff0976957ac0ab59f
ZiyangZhang <ZiyangZhang@linux.alibaba.com>
2022-11-02 14:08:05 +0800

    libublksrv: add UBLK_CMD_START_USER_RECOVERY and UBLK_CMD_END_USER_RECOVERY support
    START_USER_RECOVERY and END_USER_RECOVERY are two new control commands
    to support user recovery feature.
    
    UBLK_CMD_START_USER_RECOVERY is valid if UBLK_F_USER_RECOVERY feature
    is enabled. This command is accepted after the old process has exited,
    ublk device is quiesced and ``/dev/ublkc*`` is released. User should
    send this command before he starts a new process which re-opens
    /dev/ublkc*. When this command returns, the ublk device is ready for
    the new process.
    
    UBLK_CMD_END_USER_RECOVERY is valid if UBLK_F_USER_RECOVERY feature is
    enabled. This command is accepted after ublk device is quiesced and a
    new process has opened /dev/ublkc* and get all ublk queues be ready.
    When this command returns, ublk device is unquiesced and new I/O
    requests are passed to the new process.
    
    Note: we can handle STOP_DEV between START_USER_RECOVERY and
    END_USER_RECOVERY. This is helpful to users who cannot start new process
    after sending START_USER_RECOVERY ctrl-cmd.
    
    Signed-off-by: ZiyangZhang <ZiyangZhang@linux.alibaba.com>
    
    Avoid to change layout of ublksrv_ctrl_dev, we are starting to lock
    APIs now; rename tgt_jbuf as recovery_jbuf;
    
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

--- include/ublksrv.h
+++ include/ublksrv.h
@@ -109,8 +109,15 @@ struct ublksrv_ctrl_dev {
 	 */
 	const char *run_dir;
 
-	int tgt_argc;
-	char **tgt_argv;
+	union {
+		/* used by ->init_tgt() */
+		struct {
+			int tgt_argc;
+			char **tgt_argv;
+		};
+		/* used by ->recovery_tgt() */
+		const char *recovery_jbuf;
+	};
 
 	cpu_set_t *queues_cpuset;
 
@@ -371,6 +378,9 @@ extern int ublksrv_ctrl_set_params(struct ublksrv_ctrl_dev *dev,
 		struct ublk_params *params);
 extern int ublksrv_ctrl_get_params(struct ublksrv_ctrl_dev *dev,
 		struct ublk_params *params);
+extern int ublksrv_ctrl_start_recovery(struct ublksrv_ctrl_dev *dev);
+extern int ublksrv_ctrl_end_recovery(struct ublksrv_ctrl_dev *dev,
+		int daemon_pid);
 
 extern struct ublksrv_dev *ublksrv_dev_init(const struct ublksrv_ctrl_dev *
 		ctrl_dev);
