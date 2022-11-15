f3b7e93fd1e381eb0f96b8253dfe9d013952a81c
Ming Lei <tom.leiming@gmail.com>
2022-11-15 15:44:13 +0000

    ublksrv: add ublk_user_id utility
    Used for handling ublk udev rule when one new ublkcN/ublkbN is added.
    
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

---
+++ utils/ublk_chown.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# SPDX-License-Identifier: MIT or GPL-2.0-only
+
+MY_DIR=$(cd "$(dirname "$0")";pwd)
+ID=`${MY_DIR}/ublk_user_id $1`
+
+if [ "${ID}" != "-1:-1" ]; then
+	/usr/bin/chown $ID /dev/$1 > /dev/null 2>&1
+fi
