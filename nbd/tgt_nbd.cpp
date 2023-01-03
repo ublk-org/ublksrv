c93012e93057196c640b21c5262babf557a54211
Ming Lei <tom.leiming@gmail.com>
2023-01-03 13:41:28 +0000

    nbd: work on handshake stage
    Make handshare stage working now, socket can setup connections and
    negotiate successfully.
    
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

--- nbd/tgt_nbd.cpp
+++ nbd/tgt_nbd.cpp
@@ -2,10 +2,18 @@
 
 #include <config.h>
 #include "ublksrv_tgt.h"
+#include "cliserv.h"
+#include "nbd.h"
 
 static int nbd_init_tgt(struct ublksrv_dev *dev, int type, int argc,
 		char *argv[])
 {
+	static const struct option nbd_longopts[] = {
+		{ "host",	required_argument, 0, 0},
+		{ "unix",	required_argument, 0, 0},
+		{ "name",	required_argument, 0, 0},
+		{ NULL }
+	};
 	struct ublksrv_tgt_info *tgt = &dev->tgt;
 	const struct ublksrv_ctrl_dev_info *info =
 		ublksrv_ctrl_get_dev_info(ublksrv_get_ctrl_dev(dev));
@@ -14,34 +22,94 @@ static int nbd_init_tgt(struct ublksrv_dev *dev, int type, int argc,
 	struct ublksrv_tgt_base_json tgt_json = {
 		.type = type,
 	};
-	unsigned long long dev_size = 250UL * 1024 * 1024 * 1024;
-	struct ublk_params p = {
-		.types = UBLK_PARAM_TYPE_BASIC,
-		.basic = {
-			.logical_bs_shift	= 9,
-			.physical_bs_shift	= 12,
-			.io_opt_shift		= 12,
-			.io_min_shift		= 9,
-			.max_sectors		= info->max_io_buf_bytes >> 9,
-			.dev_sectors		= dev_size >> 9,
-		},
-	};
-	int ret;
+	int ret, i;
+
+	uint16_t flags = 0;
+	const char *port = NBD_DEFAULT_PORT;
+	uint16_t needed_flags = 0;
+	uint32_t cflags = NBD_FLAG_C_FIXED_NEWSTYLE;
+	unsigned char bs_shift = 9;
+	const char *hostname = NULL;
+	char name[128] = {0};
+	const char *unix_path = NULL;
+	u64 size64 = 0;
+	bool can_opt_go = true;
+
+	/* todo: support tls */
+	char *certfile = NULL;
+	char *keyfile = NULL;
+	char *cacertfile = NULL;
+	char *tlshostname = NULL;
+	bool tls = false;
+
+	int opt;
+	int option_index = 0;
 
 	strcpy(tgt_json.name, "nbd");
 
 	if (type != UBLKSRV_TGT_TYPE_NBD)
 		return -1;
 
-	tgt_json.dev_size = tgt->dev_size = dev_size;
+	strcpy(name, "");
+	while ((opt = getopt_long(argc, argv, "-:f:",
+				  nbd_longopts, &option_index)) != -1) {
+		if (opt < 0)
+			break;
+		if (opt > 0)
+			continue;
+
+		//fprintf(stderr, "option %s", nbd_longopts[option_index].name);
+		//if (optarg)
+		//    fprintf(stderr, " with arg %s", optarg);
+		//printf("\n");
+		if (!strcmp(nbd_longopts[option_index].name, "host"))
+		      hostname = optarg;
+		if (!strcmp(nbd_longopts[option_index].name, "unix"))
+		      unix_path = optarg;
+		if (!strcmp(nbd_longopts[option_index].name, "name"))
+			strncpy(name, optarg, sizeof(name));
+	}
+
+	for (i = 0; i < info->nr_hw_queues; i++) {
+		int sock;
+		unsigned int opts = 0;
+
+		if (unix_path != NULL)
+			sock = openunix(unix_path);
+		else
+			sock = opennet(hostname, port, false);
+
+		if (sock >= 0)
+			negotiate(&sock, &size64, &flags, name,
+					needed_flags, cflags, opts, certfile,
+					keyfile, cacertfile, tlshostname, tls,
+					can_opt_go);
+
+		tgt->fds[i + 1] = sock;
+		//fprintf(stderr, "%s:%s size %luMB flags %x sock %d\n",
+		//		hostname, port, size64 >> 20, flags, sock);
+	}
+
+	tgt_json.dev_size = tgt->dev_size = size64;
 	tgt->tgt_ring_depth = info->queue_depth;
-	tgt->nr_fds = 0;
+	tgt->nr_fds = info->nr_hw_queues;
 
 	ublksrv_tgt_set_io_data_size(tgt);
 
 	ublksrv_json_write_dev_info(ublksrv_get_ctrl_dev(dev), jbuf, jbuf_size);
 	ublksrv_json_write_target_base_info(jbuf, jbuf_size, &tgt_json);
 
+	struct ublk_params p = {
+		.types = UBLK_PARAM_TYPE_BASIC,
+		.basic = {
+			.logical_bs_shift	= bs_shift,
+			.physical_bs_shift	= 12,
+			.io_opt_shift		= 12,
+			.io_min_shift		= bs_shift,
+			.max_sectors		= info->max_io_buf_bytes >> 9,
+			.dev_sectors		= size64 >> 9,
+		},
+	};
 	do {
 		ret = ublksrv_json_write_params(&p, jbuf, jbuf_size);
 		if (ret < 0)
@@ -97,10 +165,32 @@ static int nbd_handle_io_async(const struct ublksrv_queue *q,
 	return 0;
 }
 
+static void nbd_deinit_tgt(const struct ublksrv_dev *dev)
+{
+	const struct ublksrv_tgt_info *tgt = &dev->tgt;
+	const struct ublksrv_ctrl_dev_info *info =
+		ublksrv_ctrl_get_dev_info(ublksrv_get_ctrl_dev(dev));
+	int i;
+
+	for (i = 0; i < info->nr_hw_queues; i++) {
+		int fd = tgt->fds[i + 1];
+
+		shutdown(fd, SHUT_RDWR);
+		close(fd);
+	}
+}
+
+static void nbd_usage_for_add(void)
+{
+	printf("           nbd: --host=$HOST [--port=$PORT] | --unix=$UNIX_PATH\n");
+}
+
 struct ublksrv_tgt_type  nbd_tgt_type = {
 	.handle_io_async = nbd_handle_io_async,
+	.usage_for_add	=  nbd_usage_for_add,
 	.init_tgt = nbd_init_tgt,
-	.type	= UBLKSRV_TGT_TYPE_NULL,
+	.deinit_tgt = nbd_deinit_tgt,
+	.type	= UBLKSRV_TGT_TYPE_NBD,
 	.name	=  "nbd",
 	.recovery_tgt = nbd_recovery_tgt,
 };
