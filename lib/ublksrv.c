ublk-org/ublksrv

commit a58faf0d4a4951431343d910f9384dff8b7d8598

libublksrv: move target private data into one specific data structure
So target code can define its own private data, which will be passed to
callbacks from function parameter in the following patch.

Signed-off-by: Ming Lei <tom.leiming@gmail.com>

lib/ublksrv.c
@@ -428,6 +428,7 @@ static int ublksrv_queue_cmd_buf_sz(struct ublksrv_queue *q)
 void ublksrv_queue_deinit(struct ublksrv_queue *q)
 {
 	int i;
+	int nr_ios = q->dev->tgt.extra_ios + q->q_depth;
 
 	if (q->efd > 0)
 		close(q->efd);
@@ -443,7 +444,7 @@ void ublksrv_queue_deinit(struct ublksrv_queue *q)
 		munmap(q->io_cmd_buf, ublksrv_queue_cmd_buf_sz(q));
 		q->io_cmd_buf = NULL;
 	}
-	for (i = 0; i < q->q_depth; i++) {
+	for (i = 0; i < nr_ios; i++) {
 		if (q->ios[i].buf_addr) {
 			if (q->dev->tgt.ops->free_io_buf)
 				q->dev->tgt.ops->free_io_buf(q,
@@ -452,6 +453,7 @@ void ublksrv_queue_deinit(struct ublksrv_queue *q)
 				free(q->ios[i].buf_addr);
 			q->ios[i].buf_addr = NULL;
 		}
+		free(q->ios[i].private_data);
 	}
 	q->dev->__queues[q->q_id] = NULL;
 	free(q);
@@ -563,6 +565,8 @@ struct ublksrv_queue *ublksrv_queue_init(struct ublksrv_dev *dev,
 	unsigned long off;
 	int ring_depth = depth + dev->tgt.tgt_ring_depth;
 	unsigned nr_ios = depth + dev->tgt.extra_ios;
+	int io_data_size = round_up(dev->tgt.io_data_size,
+			sizeof(unsigned long));
 
 	/*
 	 * Too many extra ios
@@ -596,7 +600,7 @@ struct ublksrv_queue *ublksrv_queue_init(struct ublksrv_dev *dev,
 	}
 
 	io_buf_size = ctrl_dev->dev_info.max_io_buf_bytes;
-	for (i = 0; i < depth; i++) {
+	for (i = 0; i < nr_ios; i++) {
 		q->ios[i].buf_addr = NULL;
 		if (dev->tgt.ops->alloc_io_buf)
 			q->ios[i].buf_addr = dev->tgt.ops->alloc_io_buf(q,
@@ -615,6 +619,9 @@ struct ublksrv_queue *ublksrv_queue_init(struct ublksrv_dev *dev,
 			goto fail;
 		}
 		q->ios[i].flags = UBLKSRV_NEED_FETCH_RQ | UBLKSRV_IO_FREE;
+		q->ios[i].private_data = malloc(io_data_size);
+
+		ublk_assert(io_data_size ^ (unsigned long)q->ios[i].private_data);
 	}
 
 	ret = ublksrv_setup_ring(ring_depth, &q->ring, IORING_SETUP_SQE128 |

include/ublksrv.h
@@ -133,15 +133,11 @@ struct ublk_io {
 #define UBLKSRV_NEED_GET_DATA		(1UL << 3)
 	unsigned int flags;
 
-	union {
-		/* result is updated after all target ios are done */
-		unsigned int result;
+	/* result is updated after all target ios are done */
+	unsigned int result;
 
-		/* current completed target io cqe */
-		int queued_tgt_io;
-	};
-	struct io_uring_cqe *tgt_io_cqe;
-	unsigned long io_data;
+	/* todo: inline private data */
+	void *private_data;
 };
 
 struct ublksrv_queue {
@@ -193,6 +189,11 @@ struct ublksrv_queue {
 	struct ublk_io ios[0];
 };
 
+static inline void *ublk_io_private_data(const struct ublksrv_queue *q, int tag)
+{
+	return q->ios[tag].private_data;
+}
+
 #define  UBLKSRV_TGT_MAX_FDS	32
 enum {
 	/* evaluate communication cost, ublksrv_null vs /dev/nullb0 */
@@ -221,7 +222,7 @@ struct ublksrv_tgt_info {
 	 * ublk_io instances can be assigned for these extra IOs.
 	 */
 	unsigned int extra_ios;
-	unsigned int pad;
+	unsigned int io_data_size;
 	const struct ublksrv_tgt_type *ops;
 
 	/*

include/ublksrv_tgt.h
@@ -112,10 +112,16 @@ struct co_io_job {
 } while (0)
 
 struct ublk_io_tgt {
-	char __reserved[sizeof(struct ublk_io) - sizeof(co_handle_type)];
 	co_handle_type co;
+	struct io_uring_cqe *tgt_io_cqe;
+	int queued_tgt_io;	/* obsolete */
 };
 
-static_assert(sizeof(struct ublk_io_tgt) == sizeof(struct ublk_io), "ublk_io is defined as wrong");
+static inline struct ublk_io_tgt *ublk_get_io_tgt_data(struct ublk_io *io)
+{
+	return (struct ublk_io_tgt *)io->private_data;
+}
+
+//static_assert(sizeof(struct ublk_io_tgt) == sizeof(struct ublk_io), "ublk_io is defined as wrong");
 
 #endif

qcow2/qcow2.h
@@ -513,17 +513,17 @@ private:
 
 	void __zero_my_cluster(Qcow2State &qs, struct ublksrv_queue *q);
 	co_io_job __zero_my_cluster_co(Qcow2State &qs,
-		struct ublksrv_queue *q, struct ublk_io *io, int tag,
+		struct ublksrv_queue *q, struct ublk_io_tgt *io, int tag,
 		Qcow2SliceMeta *m);
 
 	void __write_slices(Qcow2State &qs, struct ublksrv_queue *q);
 	co_io_job __write_slice_co(Qcow2State &qs,
 		struct ublksrv_queue *q, Qcow2SliceMeta *m,
-		struct ublk_io *io, int tag);
+		struct ublk_io_tgt *io, int tag);
 
 	void __write_top(Qcow2State &qs, struct ublksrv_queue *q);
 	co_io_job  __write_top_co(Qcow2State &qs, struct ublksrv_queue *q,
-			struct ublk_io *io, int tag);
+			struct ublk_io_tgt *io, int tag);
 
 	void __done(Qcow2State &qs, struct ublksrv_queue *q);
 	bool __need_flush(int queued);

qcow2/qcow2_flush_meta.cpp
@@ -50,7 +50,7 @@ void MetaFlushingState::add_slice_to_flush(Qcow2SliceMeta *m)
 
 co_io_job MetaFlushingState::__write_slice_co(Qcow2State &qs,
 		struct ublksrv_queue *q, Qcow2SliceMeta *m,
-		struct ublk_io *io, int tag)
+		struct ublk_io_tgt *io, int tag)
 {
 	int ret;
 	qcow2_io_ctx_t ioc(tag, q->q_id);
@@ -135,18 +135,18 @@ void MetaFlushingState::__write_slices(Qcow2State &qs,
 		tag = qs.meta_flushing.alloc_tag(q);
 		if (tag == -1)
 			return;
-		io = (struct ublk_io_tgt *)&q->ios[tag];
 		m = *it;
 		it = v1.erase(it);
 		m->get_ref();
-		io->co = __write_slice_co(qs, q, m, (struct ublk_io *)io, tag);
+		io = ublk_get_io_tgt_data(&q->ios[tag]);
+		io->co = __write_slice_co(qs, q, m, io, tag);
 	}
 }
 
 //todo: run fsync before flushing top table, and global fsync should be
 //fine, given top table seldom becomes dirty
 co_io_job MetaFlushingState::__write_top_co(Qcow2State &qs,
-		struct ublksrv_queue *q, struct ublk_io *io, int tag)
+		struct ublksrv_queue *q, struct ublk_io_tgt *io, int tag)
 {
 	int ret;
 	qcow2_io_ctx_t ioc(tag, q->q_id);
@@ -204,8 +204,8 @@ void MetaFlushingState::__write_top(Qcow2State &qs,
 	if (tag == -1)
 		return;
 
-	io = (struct ublk_io_tgt *)&q->ios[tag];
-	io->co = __write_top_co(qs, q, (struct ublk_io *)io, tag);
+	io = ublk_get_io_tgt_data(&q->ios[tag]);
+	io->co = __write_top_co(qs, q, io, tag);
 }
 
 void MetaFlushingState::__done(Qcow2State &qs, struct ublksrv_queue *q)
@@ -277,7 +277,7 @@ void MetaFlushingState::__prep_write_slice(Qcow2State &qs,
 }
 
 co_io_job MetaFlushingState::__zero_my_cluster_co(Qcow2State &qs,
-		struct ublksrv_queue *q, struct ublk_io *io, int tag,
+		struct ublksrv_queue *q, struct ublk_io_tgt *io, int tag,
 		Qcow2SliceMeta *m)
 
 {
@@ -352,8 +352,8 @@ void MetaFlushingState::__zero_my_cluster(Qcow2State &qs,
 		return;
 
 	m->get_ref();
-	io = (struct ublk_io_tgt *)&q->ios[tag];
-	io->co = __zero_my_cluster_co(qs, q, (struct ublk_io *)io, tag, m);
+	io = ublk_get_io_tgt_data(&q->ios[tag]);
+	io->co = __zero_my_cluster_co(qs, q, io, tag, m);
 }
 
 void MetaFlushingState::run_flush(Qcow2State &qs,

qcow2/qcow2_meta.cpp
@@ -635,7 +635,6 @@ int Qcow2SliceMeta::load(Qcow2State &qs, const qcow2_io_ctx_t &ioc,
 	u32 qid = ioc.get_qid();
 	u32 tag = ioc.get_tag();
 	struct ublksrv_queue *q = ublksrv_get_queue(qs.dev, qid);
-	struct ublk_io *io = &q->ios[tag];
 	struct io_uring_sqe *sqe;
 	int mio_id;
 
@@ -668,9 +667,9 @@ int Qcow2SliceMeta::load(Qcow2State &qs, const qcow2_io_ctx_t &ioc,
 	sqe->user_data = build_user_data(tag, IORING_OP_READ, mio_id + 1, 1);
 
 	ublksrv_log(LOG_INFO, "%s: queue io op %d(%llx %x %llx)"
-				" (qid %d tag %u, cmd_op %u target: %d tgt_data %d) iof %x\n",
+				" (qid %d tag %u, cmd_op %u target: %d tgt_data %d)\n",
 			__func__, sqe->opcode, sqe->off, sqe->len, sqe->addr,
-			q->q_id, tag, sqe->opcode, 1, mio_id + 1, io->flags);
+			q->q_id, tag, sqe->opcode, 1, mio_id + 1);
 	meta_log("%s %s: loading %p tag %d off %lx sz %d flags %x ref %d\n",
 			__func__, typeid(*this).name(), this, tag,
 			offset, buf_sz, flags, refcnt);

qcow2/tgt_qcow2.cpp
@@ -335,7 +335,7 @@ static inline bool l2_entry_read_as_zero(u64 entry)
 }
 
 static co_io_job __qcow2_handle_io_async(struct ublksrv_queue *q,
-		struct ublk_io *io, int tag)
+		struct ublk_io_tgt *io, int tag)
 {
 	Qcow2State *qs = dev_to_qcow2state(q->dev);
 	const struct ublksrv_io_desc *iod = ublksrv_get_iod(q, tag);
@@ -462,9 +462,9 @@ exit:
 
 static int qcow2_handle_io_async(struct ublksrv_queue *q, int tag)
 {
-	struct ublk_io_tgt *io = (struct ublk_io_tgt *)&q->ios[tag];
+	struct ublk_io_tgt *io = ublk_get_io_tgt_data(&q->ios[tag]);
 
-	io->co = __qcow2_handle_io_async(q, (struct ublk_io *)io, tag);
+	io->co = __qcow2_handle_io_async(q, io, tag);
 	return 0;
 }
 
@@ -483,7 +483,6 @@ static void qcow2_deinit_tgt(struct ublksrv_dev *dev)
 static void qcow2_tgt_io_done(struct ublksrv_queue *q, struct io_uring_cqe *cqe)
 {
 	unsigned tag = user_data_to_tag(cqe->user_data);
-	struct ublk_io *io = &q->ios[tag];
 
 	qcow2_io_log("%s: res %d qid %u tag %u, cmd_op %u\n",
 			__func__, cqe->res, q->q_id,
@@ -492,8 +491,9 @@ static void qcow2_tgt_io_done(struct ublksrv_queue *q, struct io_uring_cqe *cqe)
 	//special tag is ignored, so far it is used in sending
 	//fsync during flushing meta
 	if (tag != 0xffff) {
+		struct ublk_io_tgt *io = ublk_get_io_tgt_data(&q->ios[tag]);
 		io->tgt_io_cqe = cqe;
-		((struct ublk_io_tgt *)io)->co.resume();
+		io->co.resume();
 	}
 }
 

qcow2/utils.cpp
@@ -44,7 +44,8 @@ void IOWaiters::__mapping_meta_wakeup_all(struct ublksrv_queue *q,
 		ublksrv_log(LOG_INFO, "%s: %d my tag %d tag %d idx %x\n",
 				__func__, __LINE__, my_tag, tag, idx);
 		if (all || idx == entry_idx) {
-			struct ublk_io *__io = (struct ublk_io *)&q->ios[tag];
+			struct ublk_io_tgt *__io =
+				ublk_get_io_tgt_data(&q->ios[tag]);
 
 			it = tags.erase(it);
 			__io->tgt_io_cqe = NULL;

commit a371c5c9d82564887e11c8be639aa229bcb32555

libublksrv: fix: don't allocate io buffer for extra io slots
extra io slots are usually for handling meta io, so not need block io
buffer.

Fixes: a58faf0 ("libublksrv: move target private data into one specific data structure")
Signed-off-by: Ming Lei <tom.leiming@gmail.com>

lib/ublksrv.c
@@ -519,6 +519,11 @@ const struct ublksrv_queue *ublksrv_queue_init(const struct ublksrv_dev *tdev,
 	io_buf_size = ctrl_dev->dev_info.max_io_buf_bytes;
 	for (i = 0; i < nr_ios; i++) {
 		q->ios[i].buf_addr = NULL;
+
+		/* extra ios needn't to allocate io buffer */
+		if (i >= q->q_depth)
+			goto skip_alloc_buf;
+
 		if (dev->tgt.ops->alloc_io_buf)
 			q->ios[i].buf_addr =
 				dev->tgt.ops->alloc_io_buf(local_to_tq(q),
@@ -536,6 +541,7 @@ const struct ublksrv_queue *ublksrv_queue_init(const struct ublksrv_dev *tdev,
 					q->dev->ctrl_dev->dev_info.dev_id, q->q_id, i);
 			goto fail;
 		}
+skip_alloc_buf:
 		q->ios[i].flags = UBLKSRV_NEED_FETCH_RQ | UBLKSRV_IO_FREE;
 		q->ios[i].data.private_data = malloc(io_data_size);
 		q->ios[i].data.tag = i;