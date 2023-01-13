3930be0783f817e96014d97319c047824ef0dcc7
Ming Lei <tom.leiming@gmail.com>
2023-01-13 09:27:17 +0000

    nbd: zero recv cqe->res is only one issue in case of zero bytes done
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

--- nbd/tgt_nbd.cpp
+++ nbd/tgt_nbd.cpp
@@ -301,7 +301,7 @@ static int nbd_handle_recv_reply(const struct ublksrv_queue *q,
 				__LINE__, cqe->res);
 		ret = cqe->res;
 		goto fail;
-	} else if (cqe->res == 0) {
+	} else if (cqe->res == 0 && !nbd_data->done) {
 		//return 0;
 		nbd_err("%s %d: zero reply cqe %d %llx\n", __func__,
 				__LINE__, cqe->res, cqe->user_data);
