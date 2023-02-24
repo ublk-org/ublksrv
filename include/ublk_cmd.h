e1c8e521cfa59b91272e2c85dd79cf4a7055c072
Ming Lei <tom.leiming@gmail.com>
2023-02-24 22:54:37 +0000

    ublk_cmd.h: update with kernel uapi header
    Prepare for supporting io_uring fused command, which can help to
    implement zero copy.
    
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

--- include/ublk_cmd.h
+++ include/ublk_cmd.h
@@ -92,6 +92,8 @@
 	_IOWR('u', UBLK_IO_COMMIT_AND_FETCH_REQ, struct ublksrv_io_cmd)
 #define	UBLK_U_IO_NEED_GET_DATA		\
 	_IOWR('u', UBLK_IO_NEED_GET_DATA, struct ublksrv_io_cmd)
+#define	UBLK_U_IO_PROVIDE_IO_BUF		\
+	_IOWR('u', 0x23, struct ublksrv_io_cmd)
 
 /* only ABORT means that no re-fetch */
 #define UBLK_IO_RES_OK			0
