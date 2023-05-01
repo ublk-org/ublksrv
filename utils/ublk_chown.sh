40d524edfc2630acb095634b0cd070c667e02a39
Ming Lei <tom.leiming@gmail.com>
2023-05-01 02:53:57 +0000

    ublksrv_tgt: utils: pass more info to ublk udev script
    Prepare for supporting docker.
    
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

--- utils/ublk_chown.sh
+++ utils/ublk_chown.sh
@@ -4,6 +4,8 @@
 MY_DIR=$(cd "$(dirname "$0")";pwd)
 ID=`${MY_DIR}/ublk_user_id $1`
 
-if [ "${ID}" != "-1:-1" ]; then
-	/usr/bin/chown $ID /dev/$1 > /dev/null 2>&1
+if [ "$2" == "add" ]; then
+	if [ "${ID}" != "-1:-1" ]; then
+		/usr/bin/chown $ID /dev/$1 > /dev/null 2>&1
+	fi
 fi
