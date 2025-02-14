6fa0473991504ee70fcdc533078356dc1da84ea8
Ronnie Sahlberg <rsahlberg@whamcloud.com>
2025-02-14 15:24:18 +1000

    Add ublksrv_parse_std_opts() which will parse all standard options
    The standard options are a set of common options that apply to
    all types of backends.
    Move the parsing and populating of struct ublksrv_dev_data
    into a helper function so that we do not ave to duplicate this code
    for all targets.
    
    Signed-off-by: Ronnie Sahlberg <rsahlberg@whamcloud.com>

--- ublk.null.cpp
+++ ublk.null.cpp
@@ -111,136 +111,14 @@ static void cmd_usage(const char *name)
 	printf("\t--debug_mask=0x{DBG_MASK} --unprivileged\n\n");
 }
 
-//todo: resolve stack usage warning for mkpath/__mkpath
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wstack-usage="
-static int __mkpath(char *dir, mode_t mode)
+static int cmd_dev_add(struct ublksrv_tgt_type *tgt_type, int argc, char *argv[])
 {
-	struct stat sb;
-	int ret;
-	mode_t mask;
-
-	if (!dir)
-		return -EINVAL;
-
-	if (!stat(dir, &sb))
-		return 0;
-
-	__mkpath(dirname(strdupa(dir)), mode);
-
-	mask = umask(0);
-	ret = mkdir(dir, mode);
-	umask(mask);
-
-	return ret;
-}
-
-static int mkpath(const char *dir)
-{
-	return __mkpath(strdupa(dir), S_IRWXU | S_IRWXG | S_IRWXO);
-}
-#pragma GCC diagnostic pop
-
-static int cmd_dev_add(int argc, char *argv[])
-{
-	static const struct option longopts[] = {
-		{ "type",		1,	NULL, 't' },
-		{ "number",		1,	NULL, 'n' },
-		{ "queues",		1,	NULL, 'q' },
-		{ "depth",		1,	NULL, 'd' },
-		{ "zero_copy",		1,	NULL, 'z' },
-		{ "uring_comp",		1,	NULL, 'u' },
-		{ "need_get_data",	1,	NULL, 'g' },
-		{ "user_recovery",	1,	NULL, 'r'},
-		{ "user_recovery_fail_io",	1,	NULL, 'e'},
-		{ "user_recovery_reissue",	1,	NULL, 'i'},
-		{ "debug_mask",	1,	NULL, 0},
-		{ "unprivileged",	0,	NULL, 0},
-		{ "usercopy",	0,	NULL, 0},
-		{ NULL }
-	};
 	struct ublksrv_dev_data data = {0};
 	struct ublksrv_ctrl_dev *dev;
-	const struct ublksrv_tgt_type *tgt_type = &null_tgt_type;
-	int opt, ret;
-	int uring_comp = 0;
-	int need_get_data = 0;
-	int user_recovery = 0;
-	int user_recovery_fail_io = 0;
-	int user_recovery_reissue = 0;
-	int unprivileged = 0;
+	int ret;
 	const char *dump_buf;
 
-	data.queue_depth = DEF_QD;
-	data.nr_hw_queues = DEF_NR_HW_QUEUES;
-	data.dev_id = -1;
-	data.run_dir = ublksrv_get_pid_dir();
-
-	mkpath(data.run_dir);
-
-	while ((opt = getopt_long(argc, argv, "-:t:n:d:q:u:g:r:e:i:z",
-				  longopts, &option_index)) != -1) {
-		switch (opt) {
-		case 'n':
-			data.dev_id = strtol(optarg, NULL, 10);
-			break;
-		case 't':
-			data.tgt_type = optarg;
-			break;
-		case 'z':
-			data.flags |= UBLK_F_SUPPORT_ZERO_COPY;
-			break;
-		case 'q':
-			data.nr_hw_queues = strtol(optarg, NULL, 10);
-			break;
-		case 'd':
-			data.queue_depth = strtol(optarg, NULL, 10);
-			break;
-		case 'u':
-			uring_comp = strtol(optarg, NULL, 10);
-			break;
-		case 'g':
-			need_get_data = strtol(optarg, NULL, 10);
-			break;
-		case 'r':
-			user_recovery = strtol(optarg, NULL, 10);
-			break;
-		case 'e':
-			user_recovery_fail_io = strtol(optarg, NULL, 10);
-			break;
-		case 'i':
-			user_recovery_reissue = strtol(optarg, NULL, 10);
-			break;
-		case 0:
-			if (!strcmp(longopts[option_index].name, "debug_mask"))
-				debug_mask = strtol(optarg, NULL, 16);
-			if (!strcmp(longopts[option_index].name, "unprivileged"))
-				unprivileged = 1;
-			if (!strcmp(longopts[option_index].name, "usercopy"))
-				data.flags |= UBLK_F_USER_COPY;
-			break;
-		}
-	}
-
-	ublk_set_debug_mask(debug_mask);
-
-	data.max_io_buf_bytes = DEF_BUF_SIZE;
-	if (data.nr_hw_queues > MAX_NR_HW_QUEUES)
-		data.nr_hw_queues = MAX_NR_HW_QUEUES;
-	if (data.queue_depth > MAX_QD)
-		data.queue_depth = MAX_QD;
-	if (uring_comp)
-		data.flags |= UBLK_F_URING_CMD_COMP_IN_TASK;
-	if (need_get_data)
-		data.flags |= UBLK_F_NEED_GET_DATA;
-	if (user_recovery)
-		data.flags |= UBLK_F_USER_RECOVERY;
-	if (user_recovery_fail_io)
-		data.flags |= UBLK_F_USER_RECOVERY | UBLK_F_USER_RECOVERY_FAIL_IO;
-	if (user_recovery_reissue)
-		data.flags |= UBLK_F_USER_RECOVERY | UBLK_F_USER_RECOVERY_REISSUE;
-	if (unprivileged)
-		data.flags |= UBLK_F_UNPRIVILEGED_DEV;
+	ublksrv_parse_std_opts(&data, argc, argv);
 
 	if (data.tgt_type && strcmp(data.tgt_type, tgt_type->name)) {
 		fprintf(stderr, "Wrong tgt_type specified\n");
@@ -305,15 +183,15 @@ static int cmd_dev_add(int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
-	const char *name = "null";
+	struct ublksrv_tgt_type *tgt_type = &null_tgt_type;
 	const char *cmd = "add";
 	int ret;
 
 	setvbuf(stdout, NULL, _IOLBF, 0);
 
-	ret = cmd_dev_add(argc, argv);
+	ret = cmd_dev_add(tgt_type, argc, argv);
 	if (ret) {
-		cmd_usage(name);
+		cmd_usage(tgt_type->name);
 		ret = EXIT_FAILURE;
 	}
 
