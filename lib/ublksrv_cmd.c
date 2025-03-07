ublk-org/ublksrv

commit 8f2a56177da37f6fde109373efdf3f81c96ac4c7

Handle interrupts in __ublksrv_ctrl_cmd
The syscall io_uring_enter may return EINTR if it is interrupted by a
delivery of a signal. Since __ublksrv_ctrl_cmd already added an SQE, it's
easier for ublksrv to handle the error instead of the caller.

For example, if ublksrv_ctrl_start_dev() returns -EINTR, then the caller
has to either manually wait for the CQE or might call
ublksrv_ctrl_start_dev() again, resulting in an erroneous second SQE.
(Which may have no effect, but handling the interrupt seems the most
correct)

lib/ublksrv_cmd.c
@@ -123,7 +123,9 @@ static int __ublksrv_ctrl_cmd(struct ublksrv_ctrl_dev *dev,
 		return ret;
 	}
 
-	ret = io_uring_wait_cqe(&dev->ring, &cqe);
+	do {
+		ret = io_uring_wait_cqe(&dev->ring, &cqe);
+	} while (ret == -EINTR);
 	if (ret < 0) {
 		fprintf(stderr, "wait cqe: %s\n", strerror(-ret));
 		return ret;

commit 31b0a0e0c7384fcef12497e1881cba0e5d3266aa

fix: Handle more interrupts in __ublksrv_ctrl_cmd
Fixes: 8f2a561 ("Handle interrupts in __ublksrv_ctrl_cmd")

The syscall io_uring_enter may return EINTR if it is interrupted by a
delivery of a signal. This can stil occur on the first submit.

This commit moves the previous do-while to a combined
io_uring_submit_and_wait and updates io_uring_wait_cqe to
io_uring_peek_cqe.

lib/ublksrv_cmd.c
@@ -117,15 +117,15 @@ static int __ublksrv_ctrl_cmd(struct ublksrv_ctrl_dev *dev,
 
 	ublksrv_ctrl_init_cmd(dev, sqe, data);
 
-	ret = io_uring_submit(&dev->ring);
+	do {
+		ret = io_uring_submit_and_wait(&dev->ring, 1);
+	} while (ret == -EINTR);
 	if (ret < 0) {
-		fprintf(stderr, "uring submit ret %d\n", ret);
+		fprintf(stderr, "uring submit_and_wait ret %d\n", ret);
 		return ret;
 	}
 
-	do {
-		ret = io_uring_wait_cqe(&dev->ring, &cqe);
-	} while (ret == -EINTR);
+	ret = io_uring_peek_cqe(&dev->ring, &cqe);
 	if (ret < 0) {
 		fprintf(stderr, "wait cqe: %s\n", strerror(-ret));
 		return ret;