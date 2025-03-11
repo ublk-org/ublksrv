cf80e24b445ea2330bfbc8120ffceeec95e1ef07
Matthew Broomfield <mattysweeps@google.com>
2025-03-11 18:09:59 +0000

    Don't exit from ublksrv_ctrl_init
    ublksrv_ctrl_init can exit() if the ublk-control device can't be opened.
    This can cause ./ublk to wait indefinitely for the device_id to be
    written to the eventfd between ./ublk and the target program.
    
    - Update ublksrv_ctrl_init to return NULL on all failures
    - Update callers of ublksrv_ctrl_init to check if result is NULL.

--- lib/ublksrv_cmd.c
+++ lib/ublksrv_cmd.c
@@ -156,7 +156,8 @@ struct ublksrv_ctrl_dev *ublksrv_ctrl_init(struct ublksrv_dev_data *data)
 	dev->ctrl_fd = open(CTRL_DEV, O_RDWR);
 	if (dev->ctrl_fd < 0) {
 		fprintf(stderr, "control dev %s can't be opened: %m\n", CTRL_DEV);
-		exit(dev->ctrl_fd);
+		free(dev);
+		return NULL;
 	}
 
 	/* -1 means we ask ublk driver to allocate one free to us */
