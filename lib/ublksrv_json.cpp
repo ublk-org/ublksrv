b325abd3e0c92a6cb465fd7a33a4e32b3f3ed470
Ronnie Sahlberg <rsahlberg@whamcloud.com>
2025-03-12 15:42:41 +1000

    Make ublk_json_write_tgt_ulong public
    and flag ublksrv_json_write_target_ulong_info deprecated.
    
    Signed-off-by: Ronnie Sahlberg <rsahlberg@whamcloud.com>

--- lib/ublksrv_json.cpp
+++ lib/ublksrv_json.cpp
@@ -298,6 +298,24 @@ int ublksrv_json_write_target_ulong_info(char *jbuf, int len, const char *name,
 	return dump_json_to_buf(j, jbuf, len);
 }
 
+int ublk_json_write_tgt_ulong(const struct ublksrv_ctrl_dev *cdev, const char *name, unsigned long val)
+{
+	struct ublksrv_tgt_jbuf *j = ublksrv_tgt_get_jbuf(cdev);
+	int ret = 0;
+
+	if (!j)
+		return -EINVAL;
+
+	pthread_mutex_lock(&j->lock);
+	do {
+		ret = ublksrv_json_write_target_ulong_info(j->jbuf, j->jbuf_size,
+				name, val);
+	} while (ret < 0 && tgt_realloc_jbuf(j));
+	pthread_mutex_unlock(&j->lock);
+
+	return ret;
+}
+
 int ublksrv_json_write_target_base_info(char *jbuf, int len,
 		const struct ublksrv_tgt_base_json *tgt)
 {
