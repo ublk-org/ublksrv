ublk-org/ublksrv

commit 27e9327ad66bd8c9395b4e908eeb1c311c3bf41d

targets: loop: set UBLK_ATTR_VOLATILE_CACHE
loop target has implemented FLUSH command, so it is just fine to
set UBLK_ATTR_VOLATILE_CACHE.

And the FLUSH command handling works for both buffered or direct IO
of backing file.

Reviewed-by: Ronnie Sahlberg <rsahlberg@whamcloud.com>
Signed-off-by: Ming Lei <tom.leiming@gmail.com>

targets/ublk.loop.cpp
@@ -148,6 +148,7 @@ static int loop_init_tgt(struct ublksrv_dev *dev, int type, int argc, char
 	struct ublk_params p = {
 		.types = UBLK_PARAM_TYPE_BASIC | UBLK_PARAM_TYPE_DISCARD,
 		.basic = {
+			.attrs                  = UBLK_ATTR_VOLATILE_CACHE,
 			.logical_bs_shift	= 9,
 			.physical_bs_shift	= 12,
 			.io_opt_shift	= 12,
@@ -338,7 +339,6 @@ static void loop_queue_tgt_write(const struct ublksrv_queue *q,
 			buf, iod->nr_sectors << 9,
 			(iod->start_sector + tgt_data->offset) << 9);
 		io_uring_sqe_set_flags(sqe2, IOSQE_FIXED_FILE);
-		sqe2->rw_flags |= RWF_DSYNC;
 		/* bit63 marks us as tgt io */
 		sqe2->user_data = build_user_data(tag, ublk_op, 0, 1);
 	} else {
@@ -351,7 +351,6 @@ static void loop_queue_tgt_write(const struct ublksrv_queue *q,
 			iod->nr_sectors << 9,
 			(iod->start_sector + tgt_data->offset) << 9);
 		io_uring_sqe_set_flags(sqe, IOSQE_FIXED_FILE);
-		sqe->rw_flags |= RWF_DSYNC;
 		/* bit63 marks us as tgt io */
 		sqe->user_data = build_user_data(tag, ublk_op, 0, 1);
 	}

commit b82f7e46261590c4990a0e45e31aa342f4cbbf69

targets: loop: call fsync() for handling FLUSH command
FLUSH command from block layer doesn't specify range, so call fsync
for handle it.

Fixes 'generic/003' test failure.

Fixes: 27e9327 ("targets: loop: set UBLK_ATTR_VOLATILE_CACHE")
Signed-off-by: Ming Lei <tom.leiming@gmail.com>

targets/ublk.loop.cpp
@@ -367,9 +367,8 @@ static int loop_queue_tgt_io(const struct ublksrv_queue *q,
 	switch (ublk_op) {
 	case UBLK_IO_OP_FLUSH:
 		ublk_get_sqe_pair(q->ring_ptr, &sqe, NULL);
-		io_uring_prep_sync_file_range(sqe, 1 /*fds[1]*/,
-				iod->nr_sectors << 9,
-				(iod->start_sector + tgt_data->offset) << 9,
+		io_uring_prep_fsync(sqe,
+				1 /*fds[1]*/,
 				IORING_FSYNC_DATASYNC);
 		io_uring_sqe_set_flags(sqe, IOSQE_FIXED_FILE);
 		/* bit63 marks us as tgt io */