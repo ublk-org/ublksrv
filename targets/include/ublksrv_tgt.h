80f2cce8a0e3512fd7a8a27412d80fdf872de66d
Ronnie Sahlberg <rsahlberg@whamcloud.com>
2025-04-13 16:16:42 +1000

    Remove three functions from ublksrv_tgt.h
    Two of these functions no longer exist and the third
    inline function is not used by the codebase.
    
    Signed-off-by: Ronnie Sahlberg <rsahlberg@whamcloud.com>

--- targets/include/ublksrv_tgt.h
+++ targets/include/ublksrv_tgt.h
@@ -36,29 +36,6 @@ static inline unsigned ilog2(unsigned x)
 
 #define MAX_NR_UBLK_DEVS	128
 
-char *ublksrv_tgt_return_json_buf(struct ublksrv_dev *dev, int *size);
-char *ublksrv_tgt_realloc_json_buf(struct ublksrv_dev *dev, int *size);
-
-static inline unsigned ublksrv_convert_cmd_op(const struct ublksrv_io_desc *iod)
-{
-	unsigned ublk_op = ublksrv_get_op(iod);
-
-	switch (ublk_op) {
-	case UBLK_IO_OP_READ:
-		return IORING_OP_READ;
-	case UBLK_IO_OP_WRITE:
-		return IORING_OP_WRITE;
-	case UBLK_IO_OP_FLUSH:
-		return IORING_OP_FSYNC;
-	case UBLK_IO_OP_DISCARD:
-	case UBLK_IO_OP_WRITE_SAME:
-	case UBLK_IO_OP_WRITE_ZEROES:
-		return IORING_OP_FALLOCATE;
-	default:
-		return -1;
-	}
-}
-
 /*
  * Our convention is to use this macro instead of raw `co_await` to make it
  * easy to log `tag` when debugging coroutine issues.
